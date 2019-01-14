7b1e0086d82c6bae4a28ead69ff236d42bcc345d
waleed.tayseer <waleed.tayseer@itworx.com>
2019-01-14 15:17:26 +0200

    fdgdf

---
+++ Algorithms/Helpers/InfoUtilities.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Helpers
+{
+    public class InfoUtilities
+    {
+        public static List<int[]> FindPairsOfNumbers(List<int> arr, int total)
+        {
+            var setsOfNumbers = new List<int[]>();
+
+            for (int i = 0; i < arr.Count - 1; i++)
+            {
+                var currentNumber = arr[i];
+                for (int j = i + 1; j < arr.Count; j++)
+                {
+                    var nextNumber = arr[j];
+                    if (currentNumber + nextNumber == total)
+                        setsOfNumbers.Add(new int[2] { currentNumber, nextNumber });
+                }
+            }
+
+            return setsOfNumbers;
+        }
+
+        public static List<List<int>> FindAllSumCombinations(List<int> arr, int total)
+        {
+            var setsOfNumbers = new List<List<int>>();
+
+
+
+            return setsOfNumbers;
+        }
+
+        private void _subsetSum(List<int> numbers, int target, List<int> partial)
+        {
+            var s = partial.Aggregate(0, (a, b) => { return a + b; });
+
+            if (s == target)
+            {
+                var lol = 0;
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Algorithms.Helpers;
 
 namespace Algorithms
 {
@@ -6,15 +7,18 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            // Distinct values
-            var arr = new List<int>() { 1, 1, 2, 2, 3, 4 };
-            List<int> distinctValues = ArrayUtilities.GetDistinctValues(arr);
+            //var arr = new List<int>() { 1, 1, 2, 2, 3, 4 };
+            //List<int> distinctValues = ArrayUtilities.GetDistinctValues(arr);
 
-            var unsortedArray = new List<int> { 3, 2, 6, 4, 7, 1 };
-            var sortedArray = ArrayUtilities.SortAscending(unsortedArray);
+            //var unsortedArray = new List<int> { 3, 2, 6, 4, 7, 1 };
+            //var sortedArray = ArrayUtilities.SortAscending(unsortedArray);
 
-            var decimalUnsortedArray = new List<decimal>() { 0.897M, 0.565M, 0.656M, 0.1234M, 0.665M, 0.3434M };
-            var decimalSortedArray = ArrayUtilities.BucketSort(decimalUnsortedArray);
+            //var decimalUnsortedArray = new List<decimal>() { 0.897M, 0.565M, 0.656M, 0.1234M, 0.665M, 0.3434M };
+            //var decimalSortedArray = ArrayUtilities.BucketSort(decimalUnsortedArray);
+
+            var setsOfNumbers = new List<int> { 2, 4, 6, 10 };
+            var pairs = InfoUtilities.FindPairsOfNumbers(setsOfNumbers, 16);
+
         }
     }
 }
