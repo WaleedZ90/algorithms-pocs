b07f31040fa5c0a97ae83f405b7c10760935f3d2
waleed.tayseer <waleed.tayseer@itworx.com>
2019-01-14 10:30:34 +0200

    initial commit

---
+++ Algorithms/ArrayUtilities/ArrayUtilities.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class ArrayUtilities
+    {
+        // Reference: https://www.geeksforgeeks.org/print-distinct-elements-given-integer-array/
+        public static List<int> GetDistinctValues(List<int> arr)
+        {
+            var distinctValues = new List<int>();
+
+            //int i;
+
+            //// Traverse the sorted array 
+            //for (i = 0; i < arr.Count; i++)
+            //{
+            //    // Move the index ahead while  
+            //    // there are duplicates 
+            //    while (i < arr.Count - 1 && arr[i] == arr[i + 1])
+            //        i++;
+
+            //    // add last occurrence of  
+            //    // the current element 
+            //    distinctValues.Add(arr[i]);
+            //}
+
+            var set = new HashSet<int>();
+            foreach (int number in arr)
+            {
+                if (!set.Contains(number))
+                {
+                    set.Add(number);
+                    distinctValues.Add(number);
+                }
+            }
+
+            return distinctValues;
+        }
+        public static List<int> SortAscending(List<int> arr)
+        {
+            for (int i = 0; i < arr.Count - 1; i++)
+            {
+                int min_index = i;
+                for (int j = i + 1; j < arr.Count; j++)
+                {
+                    var min = arr[min_index];
+                    var current = arr[j];
+                    if (arr[j] < arr[min_index])
+                    {
+                        min_index = j;
+                    }
+
+                    int tempValue = arr[min_index];
+                    arr[min_index] = arr[i];
+                    arr[i] = tempValue;
+                }
+            }
+            return arr;
+        }
+
+        public static List<decimal> BucketSort(List<decimal> arr)
+        {
+            var bucketsDictionary = new Dictionary<int, List<decimal>>();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                bucketsDictionary.Add(i, new List<decimal>());
+            }
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                var bucketNumber = _getBucketNumber(arr[i]);
+                bucketsDictionary[bucketNumber].Add(arr[i]);
+            }
+
+            return arr;
+        }
+
+        #region Helpers
+        private static int _getBucketNumber(decimal value)
+        {
+            var result = value - Math.Truncate(value);
+            return (int)Math.Floor(result * 10);
+        }
+        #endregion
+    }
+
+
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Distinct values
+            var arr = new List<int>() { 1, 1, 2, 2, 3, 4 };
+            List<int> distinctValues = ArrayUtilities.GetDistinctValues(arr);
+
+            var unsortedArray = new List<int> { 3, 2, 6, 4, 7, 1 };
+            var sortedArray = ArrayUtilities.SortAscending(unsortedArray);
+
+            var decimalUnsortedArray = new List<decimal>() { 0.897M, 0.565M, 0.656M, 0.1234M, 0.665M, 0.3434M };
+            var decimalSortedArray = ArrayUtilities.BucketSort(decimalUnsortedArray);
+        }
+    }
+}
